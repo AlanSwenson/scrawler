e5e41e989c93beddd1d307896f2db59ec51d01ec
AlanSwenson <alan@alanswenson.dev>
2023-04-26 15:13:55 -0500

    fine tune crawler

--- main/lib/crawler.js
+++ main/lib/crawler.js
@@ -4,23 +4,33 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function getRoot(url) {
+export function getRoot(url) {
     var re = new RegExp(/^.+?[^\/:](?=[?\/]|$)/);
     var match = re.exec(url);
     if (match) {
-        console.log(match[0]);
-        return match[0];
+        return match[0].replace('www.', '')
     }
-
 }
 
-export async function crawl(url, maxPages = 5) {
+function compareRoot(url, root) {
+    var newRoot = getRoot(url);
+    console.log("newRoot: ", newRoot, "root: ", root);
+    if (newRoot == root) return true;
+}
+
+function isShop($) {
+    let shop = 'no'
+    if ($('.woocommerce').length) shop = 'woocommerce'
+    return shop;
+}
+
+export async function crawl(url, maxPages = 100) {
     // initialized with the first webpage to visit 
     // test url is https://scrapeme.live/shop
+    if (!url.startsWith('http') && !url.startsWith('https')) url = 'https://' + url;
     const URLsToVisit = [url];
     const visitedURLs = [];
     const rootURLs = new Set();
-    const productURLs = new Set();
 
     const config = {
         headers: {
@@ -32,69 +42,55 @@ export async function crawl(url, maxPages = 5) {
     // or the iteration limit is hit 
     while (
         URLsToVisit.length !== 0 &&
-        visitedURLs.length <= maxPages
+        visitedURLs.length < maxPages
     ) {
         // the current webpage to crawl 
         const nextURL = URLsToVisit.pop();
-        let root = await getRoot(nextURL);
+        let root = getRoot(nextURL);
 
         rootURLs.add(root);
 
         // retrieving the HTML content from nextURL 
-        const pageHTML = await axios.get(nextURL, config);
+        try {
+            const pageHTML = await axios.get(nextURL, config);
+            // adding the current webpage to the 
+            // web pages already crawled 
+            visitedURLs.push(nextURL);
+            console.log("Visited: ", visitedURLs.length);
+            // initializing cheerio on the current webpage 
+            const $ = cheerio.load(pageHTML.data);
+            console.log("Shop? ", isShop($));
 
-        // adding the current webpage to the 
-        // web pages already crawled 
-        visitedURLs.push(nextURL);
+            $("a").each((i, element) => {
+                let href = $(element).attr("href");
+                let validURL = false
+                if (href && href.startsWith('http')) {
+                    validURL = !(compareRoot(href, root));
+                }
 
-        // initializing cheerio on the current webpage 
-        const $ = cheerio.load(pageHTML.data);
+                if (
+                    validURL &&
+                    !URLsToVisit.includes(href) &&
+                    !visitedURLs.includes(href)
+                ) {
+                    console.log("adding: ", href);
+                    URLsToVisit.push(href)
+                }
 
-        $("a").each((i, element) => {
-            let href = $(element).attr("href");
+            });
+        } catch (error) {
+            console.error(error);
+        }
 
 
-            if (
-                !href == '' &&
-                !href.startsWith('#') &&
-                !URLsToVisit.includes(href) &&
-                !visitedURLs.includes(href)
-            ) {
-                console.log(href);
-                URLsToVisit.push(href)
-            }
 
-        });
-        console.log(URLsToVisit);
-        // let allLinks = await findAllLinks($);
 
-        // retrieving the pagination URLs 
-        // $(".page-numbers a").each((index, element) => {
-        //     const nextURL = $(element).attr("href");
-
-        //     // adding the pagination URL to the queue 
-        //     // of web pages to crawl, if it wasn't yet crawled 
-        //     if (
-        //         !visitedURLs.includes(nextURL) &&
-        //         !URLsToVisit.includes(nextURL)
-        //     ) {
-        //         URLsToVisit.push(nextURL);
-        //     }
-        // });
-
-        // retrieving the product URLs 
-        // $("li.product a.woocommerce-LoopProduct-link").each((index, element) => {
-        //     const productURL = $(element).attr("href");
-        //     productURLs.add(productURL);
-        // });
     }
-
-    // logging the crawling results 
-    // console.log([...productURLs]);
 
     // use productURLs for scraping purposes... 
     await prisma.rootURL.createMany({
-        data: [...rootURLs].map(url => ({ url }))
+        data: [...rootURLs].map(url => ({ url: url.replace('https://', '').replace('http://', '') })),
+        skipDuplicates: true,
     })
 
 }
