7a9d89baa02f460bf66a0aca513ac5baf4ce62e9
AlanSwenson <alan@alanswenson.dev>
2023-04-26 13:19:40 -0500

    only save root urls

--- main/lib/crawler.js
+++ main/lib/crawler.js
@@ -4,12 +4,22 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function crawl(url, maxPages = 50) {
+export async function getRoot(url) {
+    var re = new RegExp(/^.+?[^\/:](?=[?\/]|$)/);
+    var match = re.exec(url);
+    if (match) {
+        console.log(match[0]);
+        return match[0];
+    }
+
+}
+
+export async function crawl(url, maxPages = 5) {
     // initialized with the first webpage to visit 
     // test url is https://scrapeme.live/shop
-    const paginationURLsToVisit = [url];
+    const URLsToVisit = [url];
     const visitedURLs = [];
-
+    const rootURLs = new Set();
     const productURLs = new Set();
 
     const config = {
@@ -21,49 +31,70 @@ export async function crawl(url, maxPages = 50) {
     // iterating until the queue is empty 
     // or the iteration limit is hit 
     while (
-        paginationURLsToVisit.length !== 0 &&
+        URLsToVisit.length !== 0 &&
         visitedURLs.length <= maxPages
     ) {
         // the current webpage to crawl 
-        const paginationURL = paginationURLsToVisit.pop();
+        const nextURL = URLsToVisit.pop();
+        let root = await getRoot(nextURL);
 
-        // retrieving the HTML content from paginationURL 
-        const pageHTML = await axios.get(paginationURL, config);
+        rootURLs.add(root);
+
+        // retrieving the HTML content from nextURL 
+        const pageHTML = await axios.get(nextURL, config);
 
         // adding the current webpage to the 
         // web pages already crawled 
-        visitedURLs.push(paginationURL);
+        visitedURLs.push(nextURL);
 
         // initializing cheerio on the current webpage 
         const $ = cheerio.load(pageHTML.data);
 
-        // retrieving the pagination URLs 
-        $(".page-numbers a").each((index, element) => {
-            const paginationURL = $(element).attr("href");
+        $("a").each((i, element) => {
+            let href = $(element).attr("href");
 
-            // adding the pagination URL to the queue 
-            // of web pages to crawl, if it wasn't yet crawled 
+
             if (
-                !visitedURLs.includes(paginationURL) &&
-                !paginationURLsToVisit.includes(paginationURL)
+                !href == '' &&
+                !href.startsWith('#') &&
+                !URLsToVisit.includes(href) &&
+                !visitedURLs.includes(href)
             ) {
-                paginationURLsToVisit.push(paginationURL);
+                console.log(href);
+                URLsToVisit.push(href)
             }
+
         });
+        console.log(URLsToVisit);
+        // let allLinks = await findAllLinks($);
+
+        // retrieving the pagination URLs 
+        // $(".page-numbers a").each((index, element) => {
+        //     const nextURL = $(element).attr("href");
+
+        //     // adding the pagination URL to the queue 
+        //     // of web pages to crawl, if it wasn't yet crawled 
+        //     if (
+        //         !visitedURLs.includes(nextURL) &&
+        //         !URLsToVisit.includes(nextURL)
+        //     ) {
+        //         URLsToVisit.push(nextURL);
+        //     }
+        // });
 
         // retrieving the product URLs 
-        $("li.product a.woocommerce-LoopProduct-link").each((index, element) => {
-            const productURL = $(element).attr("href");
-            productURLs.add(productURL);
-        });
+        // $("li.product a.woocommerce-LoopProduct-link").each((index, element) => {
+        //     const productURL = $(element).attr("href");
+        //     productURLs.add(productURL);
+        // });
     }
 
     // logging the crawling results 
-    console.log([...productURLs]);
+    // console.log([...productURLs]);
 
     // use productURLs for scraping purposes... 
     await prisma.rootURL.createMany({
-        data: [...productURLs].map(url => ({ url }))
+        data: [...rootURLs].map(url => ({ url }))
     })
 
 }
