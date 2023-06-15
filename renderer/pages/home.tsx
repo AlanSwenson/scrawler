38516a87f8afb57785d600e91f1fb4ea681235b4
AlanSwenson <alan@alanswenson.dev>
2023-06-15 15:51:21 -0500

    styling

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -5,6 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 import localFont from '@next/font/local'
+import Button from '../components/button';
 
 const championship = localFont({
   src: './Championship.ttf',
@@ -21,25 +22,42 @@ const Home: FC = () => {
   const [crawling, setCrawling] = useState(false);
   const [searching, setSearching] = useState(false);
 
-  const crawl = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const textFieldClass = 'focus:outline-none focus:border-purple-300 border border-mixed-500 rounded-3xl px-4 py-1 mr-4 placeholder:text-mixed-500 text-white bg-dark-200';
+
+  const crawl = (event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
     setCrawling(true);
     ipcRenderer.send('start-crawl', url.toLowerCase())
   };
-  const handleSearch = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  const handleSearch = (event: React.MouseEvent<HTMLDivElement, MouseEvent> | React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
     setSearching(true);
     ipcRenderer.send('start-search', search);
   };
 
+  useEffect(() => {
+    // register `ping-pong` event
+    ipcRenderer.on('start-search', (event, data) => {
+      setSearch('');
+      setSearching(false);
+      console.log("start-search: ");
+    });
+
+    return () => {
+      // unregister it
+      console.log("unregistering start-search");
+      ipcRenderer.removeAllListeners('start-search');
+    };
+  }, []);
+
   const createMessage = (data: any) => {
     return (
       <div>
-        <div className='text-lg text-orange-500 border-b-2 border-dashed border-orange-600'>{data.search } crawl found {data.urls?.length || 0} new Urls</div>
+        <div className='mb-4 text-lg text-primary-300 border-b-2 border-dashed border-primary-100'><span className='text-2xl font-bold'>{data.search }</span> crawl found {data.urls?.length || 0} new Urls</div>
         {data.urls?.map((url: { url: string }, i: number) => {
           return (
             <div key={i}>
-              <div>{url.url}</div>
+              <div className='text-purple-300'>{url.url}</div>
             </div>
           )
         })}
@@ -47,13 +65,13 @@ const Home: FC = () => {
     )
   }
 
-
   useEffect(() => {
     // register `ping-pong` event
     ipcRenderer.on('start-crawl', (event, data) => {
       const newMessage = createMessage(data);
       setMessage(newMessage);
       setCrawling(false);
+      setUrl('');
       console.log("start-crawl: ");
     });
 
@@ -70,21 +88,25 @@ const Home: FC = () => {
       <Head>
         <title>Scrawler by Alan</title>
       </Head>
-      <div className={`${championship.variable} w-full flex flex-col justify-start h-screen bg-slate-950`}>
-        <div className='w-full text-8xl flex justify-center text-orange-400 pt-20 pb-12 font-championship uppercase align-baseline'>
+      <div className={`${championship.variable} w-full flex flex-col justify-start h-screen bg-dark-100`}>
+        <div className='w-full text-8xl flex justify-center text-primary-200 pt-20 pb-12 font-championship uppercase align-baseline'>
         Scrawler
         </div>
         <div className='w-auto mx-auto'>
         <div className='flex flex-col justify-center items-center w-full '>
           <form className='flex w-full'>
-            <input className='border rounded px-2 py-1 mr-4 text-slate-500 ' type='text' placeholder='Enter URL' value={url} onChange={e => setUrl(e.target.value)} />
-            <button className='border rounded px-4 py-1 text-white bg-green-700' onClick={(event) => crawl(event) }>Crawl</button>
+              <input className={`${textFieldClass}`} type='text' placeholder='Enter URL' value={url} onChange={e => setUrl(e.target.value)} />
+              <div onClick={(event) => crawl(event)}>
+              <Button label='Crawl'  />
+              </div>
           </form>
           <form className='flex pt-2 w-full'>
-            <input className='border rounded px-2 py-1 mr-4 text-slate-500 ' type='text' placeholder='Product Search' value={search} onChange={e => setSearch(e.target.value)} />
-            <button className='border rounded px-4 py-1 text-white bg-green-700' onClick={(event) => handleSearch(event)}>Search Amazon</button>
+          <input className={`${textFieldClass}`} type='text' placeholder='Product Search' value={search} onChange={e => setSearch(e.target.value)} />
+              <div onClick={(event) => handleSearch(event)}>
+              <Button label='Search Amazon'  />
+              </div>
           </form>
-          <div className='text-white text-center mt-4'>
+          <div className='text-white text-center my-8'>
             {crawling ? <FontAwesomeIcon icon={faSpinner} className='animate-spin' /> : message}
             </div>
         </div>   
